613d0c3c2aa38d3601e2c7fcc80a2d4f0a62f4f4
Trang <nguyenminhtrang9213@gmail.com>
2025-11-28 20:40:45 +0100

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,10 @@ import React from "react";
 function Card({ project }) {
   const { id, category, title, image_1, desc, github, demo } = project;
   return (
-    <div className=" w-full lg:w-1/4 rounded-sm p-4 bg-amber-500 shadow-lg ">
+    <div className=" w-full lg:w-1/3 xl:w-1/4 2xl:w-1/6 rounded-md p-4 bg-gradient-to-b from-red-100 to-fuchsia-100 shadow-lg hover:scale-105 transition duration-150 hover:shadow-2xl">
       {/* Image  */}
-      <div className="aspect-video relative shadow-md shadow-black/10 mb-4">
-        <Image fill src={image_1} className="object-cover" />
-        <div className="absolute inset-0 bg-black/10 mix-blend-multiply"></div>
+      <div className="aspect-video relative mb-4">
+        <Image fill src={image_1} className="object-cover rounded-md" />
       </div>
       {/* Heading  */}
       <div className="p-2 text-left flex flex-col gap-4">
@@ -21,7 +20,7 @@ function Card({ project }) {
             className="flex justify-center items-center text-sm lg:text-md py-4 px-4 bg-slate-600 w-full lg:w-1/2 shadow-lg hover:scale-105 transition-all duration-150 text-amber-100 hover:font-bold"
             href={github}
           >
-            Source Code
+            View Code
           </a>
           <a
             className="flex justify-center items-center py-3 text-sm lg:text-md px-4 bg-transparen w-full lg:w-1/2 border-2 border-slate-600 shadow-lg hover:scale-105 hover:font-bold transition-all duration-150 text-slate-600"
